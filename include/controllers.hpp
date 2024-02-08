fc41fc0ea03a9b0357da509783bf782fd9359fa3
Shobuj Paul <shobuj.paul@gmail.com>
2024-02-09 01:57:38 +0530

    pid_controller also implemented

--- include/controllers.hpp
+++ include/controllers.hpp
@@ -5,10 +5,23 @@ struct pi_config
   float Kp, Ki, Ts, upper_limit, lower_limit;
 };
 
+struct pid_config
+{
+  float Kp, Ki, Kd, Ts, upper_limit, lower_limit;
+};
+
 class pi_controller {
   float error_sum, output, error_prev;
   public:
   pi_controller();
   float clamp(float input, float upper_limit, float lower_limit);
   float loop(float error, const pi_config& config);
+};
+
+class pid_controller {
+  float error_sum, output, error_prev;
+  public:
+  pid_controller();
+  float clamp(float input, float upper_limit, float lower_limit);
+  float loop(float error, const pid_config& config);
 };
--- src/controllers.cpp
+++ src/controllers.cpp
@@ -25,3 +25,30 @@ float pi_controller::loop(float error, const pi_config& config) {
 
   return output;
 }
+
+pid_controller::pid_controller() : error_sum(0), output(0), error_prev(0) {}
+
+float pid_controller::clamp(float input, float upper_limit, float lower_limit) {
+  if(input < lower_limit)
+    return lower_limit;
+  else if(input > upper_limit)
+    return upper_limit;
+  else
+    return input;
+}    
+
+float pid_controller::loop(float error, const pid_config& config) {
+
+  if(error > 0 && output >= config.upper_limit)
+    error = 0;
+  else if(error < 0 && output <= config.lower_limit)
+    error = 0;
+  
+  error_sum += clamp(config.Ki*config.Ts*(error + error_prev)/2, config.upper_limit, config.lower_limit);
+  float derror = (error - error_prev)/config.Ts;
+  output = clamp(config.Kp*error + config.Ki*error_sum + config.Kd*derror, config.upper_limit, config.lower_limit);
+
+  error_prev = error;
+
+  return output;
+}
